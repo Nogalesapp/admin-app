d2958c9208010f400228fd0e6d5d955d22c0c1aa
Nogalesapp <appnogalesver@gmail.com>
2019-12-26 23:51:57 -0600

    Updates

---
+++ precache-manifest.c3af283592e3ebfebe75548b5b91ba7d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bf849ce114af5b51a8e41b3fe53486b",
+    "url": "/admin-app/index.html"
+  },
+  {
+    "revision": "3eac620a528d75d18abb",
+    "url": "/admin-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "b74d196cfea3ad688c9b",
+    "url": "/admin-app/static/js/2.8a4cad42.chunk.js"
+  },
+  {
+    "revision": "3eac620a528d75d18abb",
+    "url": "/admin-app/static/js/main.ad290ca2.chunk.js"
+  },
+  {
+    "revision": "f2c3138e8c36473d28df",
+    "url": "/admin-app/static/js/runtime-main.69a5868a.js"
+  }
+]);
